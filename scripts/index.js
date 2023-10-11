6860e3bce569d74244680bbc134e18f7686a2f1b
unknown <alfonsoreviejovalle@alumnos.tuniversformacion.es>
2023-10-11 09:20:54 +0200

    Update HTML, CSS, JS

---
+++ scripts/index.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", ()=> {
+
+    const button = document.getElementById("button");
+    const result = document.getElementById("result-container");
+
+    button.addEventListener("click", () => {
+        const length = parseFloat(document.getElementById("length").value);
+        const width = parseFloat(document.getElementById("width").value);
+
+        if(!isNaN(length) && !isNaN(width)){
+
+            let area = length * width;
+            let perymetre = 2 * (length + width);
+            let diametre = Math.sqrt(Math.pow(length, 2) + Math.pow(width, 2));
+    
+            result.innerHTML = `<p> El area del rectangulo es: ${area} </p>
+            <p> El perimetro ea del rectangulo es: ${perymetre} </p>
+            <p> El diametro del rectangulo es: ${diametre} </p>`;
+
+        }else{
+            result.innerHTML = `<p>Por favor, ingrese valores válidos</p>`;
+        }
+    });
+
+});
